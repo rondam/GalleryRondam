7c82d27cf23d05f0566023f2e7934f7e67047d11
rondam <marina.ronda.m@gmail.com>
2015-03-13 02:09:10 +0000

    noFunciona
    Pruebas

---
+++ application/controllers/admin_login.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+        // Check for user login process
+        public function admin_login_process()
+        {
+            if (password_verify('admin', $this)) {
+                echo '¡La contraseña es válida!';
+            } else {
+                echo 'La contraseña no es válida.';
+            }
+        }
+}
---
+++ application/views/admin_login.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Secure Login: Log In</title>
+        <form action="base_url().'admin'" method="index()" name="admin_login">
+            <input type="password" name="password" id="password"/>
+            <input type="button" value="Login" />
+        </form>
+    </head>
+    <body>
